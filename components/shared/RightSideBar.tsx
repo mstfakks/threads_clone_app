2e5531ed6cd4391c362336017eadf94ea91a6ca5
mstfakks <msfa.akkas@gmail.com>
2023-08-19 02:09:47 +0300

    Left sidebar, right sidebar, bottom bar completed

---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,3 @@
+export default function RightSideBar() {
+  return <h1>Right</h1>;
+}
---
+++ components/shared/index.ts
@@ -0,0 +1,6 @@
+import TopBar from "./TopBar";
+import LeftSideBar from "./LeftSideBar";
+import RightSideBar from "./RightSideBar";
+import BottomBar from "./BottomBar";
+
+export { TopBar, LeftSideBar, RightSideBar, BottomBar };
