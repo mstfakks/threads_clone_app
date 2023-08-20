719e3974a36290c0d44243f636387616db2d3317
mstfakks <msfa.akkas@gmail.com>
2023-08-20 14:42:13 +0300

    Create thread function completed

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,3 +1,72 @@
-export default function PostThread() {
-  return <h1>Post Thread</h1>;
+"use client";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "../ui/button";
+import { z } from "zod";
+import { Textarea } from "../ui/textarea";
+//import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.actions";
+export default function PostThread({ userId }: { userId: string }) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
+    });
+    router.push("/");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full gap-3 flex-col">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea rows={15} {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
 }
---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface ThreadProps {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: ThreadProps) {
+  try {
+    connectToDB();
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
+
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (err: any) {
+    throw new Error(`Failed to create thread: ${err.message}`);
+  }
+}
