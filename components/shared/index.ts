69028d604f7c330e38840b2a7ec0e55d34d330dc
mstfakks <msfa.akkas@gmail.com>
2023-08-24 00:27:12 +0300

    Profile page started --akkas

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,5 +2,6 @@ import TopBar from "./TopBar";
 import LeftSideBar from "./LeftSideBar";
 import RightSideBar from "./RightSideBar";
 import BottomBar from "./BottomBar";
+import ProfileHeader from "./ProfileHeader";
 
-export { TopBar, LeftSideBar, RightSideBar, BottomBar };
+export { TopBar, LeftSideBar, RightSideBar, BottomBar, ProfileHeader };
